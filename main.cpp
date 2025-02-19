6000a7f7b8941826717fde78bbede1bb7318f7d2
Jason <Jason@VS>
2025-02-18 19:22:54 -0500

    basic req fulfilled

--- main.cpp
+++ main.cpp
@@ -43,15 +43,15 @@ VIEWPORT_Y = 0,
 VIEWPORT_WIDTH = WINDOW_WIDTH,
 VIEWPORT_HEIGHT = WINDOW_HEIGHT;
 
-int END_STATE = 0;
+int GAME_STATE = 0;
 
-constexpr float SCALE_Y = 1.5f;
-constexpr float SCALE_X = SCALE_Y;
-constexpr float INIT_X = 5.0f - (SCALE_X / 2);
+constexpr float SCALE_Y = 1.5f,
+                SCALE_X = SCALE_Y,
+                INIT_X = 5.0f - (SCALE_X / 2);
 
-constexpr float UPPER_BOUND = 3.5f;
-constexpr float LOWER_BOUND = -UPPER_BOUND;
-constexpr float WALL = 4.0f;
+constexpr float UPPER_BOUND = 3.5f,
+                LOWER_BOUND = -UPPER_BOUND,
+                WALL = 4.0f;
 
 constexpr char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
 F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
@@ -59,41 +59,46 @@ F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
 constexpr float MILLISECONDS_IN_SECOND = 1000.0;
 bool SINGLE_PLAYER = false; // set default player mode to 2
 
-constexpr char LEFT_SPRITE_FILEPATH[] = "assets/cat_left.png";
-constexpr char RIGHT_SPRITE_FILEPATH[] = "assets/cat_right.png";
-constexpr char BALL_SPRITE_FILEPATH[] = "assets/image.png";
+constexpr char LEFT_SPRITE_FILEPATH[] = "assets/cat_left.png",
+               RIGHT_SPRITE_FILEPATH[] = "assets/cat_right.png",
+               BALL_SPRITE_FILEPATH[] = "assets/image.png",
+               P1_SPRITE_FILEPATH[] = "assets/p1_win.png",
+               P2_SPRITE_FILEPATH[] = "assets/p2_win.png";
 
 constexpr float MINIMUM_COLLISION_DISTANCE = 1.0f;
-// leek sword is 1 to 5 ratio
-constexpr glm::vec3 INIT_SCALE_LEFT = glm::vec3(SCALE_X, SCALE_Y, 1.0f);
-constexpr glm::vec3 INIT_SCALE_RIGHT = glm::vec3(SCALE_X, SCALE_Y, 1.0f);
-constexpr glm::vec3 INIT_SCALE_BALL = glm::vec3(0.5f, 0.5f, 1.0f);
 
-constexpr glm::vec3 INIT_POS_LEFT = glm::vec3(-INIT_X, 0.0f, 1.0f);
-constexpr glm::vec3 INIT_POS_RIGHT = glm::vec3(INIT_X, 0.0f, 1.0f);
-constexpr glm::vec3 INIT_POS_BALL = glm::vec3(0.0f, 0.0f, 0.0f);
+constexpr glm::vec3 INIT_SCALE_LEFT = glm::vec3(SCALE_X, SCALE_Y, 1.0f),
+                    INIT_SCALE_RIGHT = glm::vec3(SCALE_X, SCALE_Y, 1.0f),
+                    INIT_SCALE_BALL = glm::vec3(0.5f, 0.5f, 1.0f),
+                    INIT_SCALE_WIN = glm::vec3(8.0f, 4.0f, 1.0f);
+
+constexpr glm::vec3 INIT_POS_LEFT = glm::vec3(-INIT_X, 0.0f, 1.0f),
+                    INIT_POS_RIGHT = glm::vec3(INIT_X, 0.0f, 1.0f),
+                    INIT_POS_BALL = glm::vec3(0.0f, 0.0f, 0.0f),
+                    INIT_POS_WIN = glm::vec3(0.0f, 0.0f, 0.0f);
 
 
 SDL_Window* g_display_window;
 
 AppStatus g_app_status = RUNNING;
 ShaderProgram g_shader_program = ShaderProgram();
-glm::mat4 g_view_matrix, g_projection_matrix, g_left_matrix, g_right_matrix, g_ball_matrix;
+glm::mat4 g_view_matrix, g_projection_matrix, g_left_matrix, g_right_matrix, g_ball_matrix, g_win_matrix;
 
 float g_previous_ticks = 0.0f;
 
-GLuint g_left_texture_id;
-GLuint g_right_texture_id;
-GLuint g_ball_texture_id;
+GLuint g_left_texture_id,
+       g_right_texture_id,
+       g_ball_texture_id,
+       g_p1_texture_id,
+       g_p2_texture_id;
 
-glm::vec3 g_left_position = glm::vec3(0.0f, 0.0f, 0.0f);
-glm::vec3 g_left_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 g_left_position = glm::vec3(0.0f, 0.0f, 0.0f),
+          g_right_position = glm::vec3(0.0f, 0.0f, 0.0f),
+          g_ball_position = glm::vec3(0.0f, 0.0f, 0.0f);
 
-glm::vec3 g_right_position = glm::vec3(0.0f, 0.0f, 0.0f);
-glm::vec3 g_right_movement = glm::vec3(0.0f, 0.0f, 0.0f);
-
-glm::vec3 g_ball_position = glm::vec3(0.0f, 0.0f, 0.0f);
-glm::vec3 g_ball_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 g_left_movement = glm::vec3(0.0f, 0.0f, 0.0f),
+          g_right_movement = glm::vec3(0.0f, 0.0f, 0.0f),
+          g_ball_movement = glm::vec3(0.0f, 0.0f, 0.0f);
 
 float g_ball_speed = 1.0f;  // move 1 unit per second
 float g_paddle_speed = 2.5f;
@@ -106,6 +111,7 @@ void shutdown();
 
 // declarations cause c++
 void restart();
+void coord(glm::vec3&);
 
 constexpr GLint NUMBER_OF_TEXTURES = 1;  // to be generated, that is
 constexpr GLint LEVEL_OF_DETAIL = 0;  // base image level; Level n is the nth mipmap reduction image
@@ -166,6 +172,7 @@ void initialise()
     g_left_matrix = glm::mat4(1.0f);
     g_right_matrix = glm::mat4(1.0f);
     g_ball_matrix = glm::mat4(1.0f);
+    g_win_matrix = glm::mat4(1.0f);
 
     g_view_matrix = glm::mat4(1.0f);
     g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
@@ -180,12 +187,13 @@ void initialise()
     g_left_texture_id = load_texture(LEFT_SPRITE_FILEPATH);
     g_right_texture_id = load_texture(RIGHT_SPRITE_FILEPATH);
     g_ball_texture_id = load_texture(BALL_SPRITE_FILEPATH);
+    g_p1_texture_id = load_texture(P1_SPRITE_FILEPATH);
+    g_p2_texture_id = load_texture(P2_SPRITE_FILEPATH);
 
 
     // initial ball movement
-    g_ball_movement.x = 1.0f;
-    g_ball_movement.y = 1.0f;
-    g_ball_movement = glm::normalize(g_ball_movement);
+    srand(std::time(nullptr)); // seed with random to ensure randomness
+    restart();
 
     // enable blending
     glEnable(GL_BLEND);
@@ -243,24 +251,27 @@ void process_input()
 
     const Uint8* key_state = SDL_GetKeyboardState(NULL);
 
-    if (key_state[SDL_SCANCODE_W])
+    if (GAME_STATE == 0)
     {
-        g_left_movement.y += 1.0f;
-    }
-    if (key_state[SDL_SCANCODE_S])
-    {
-        g_left_movement.y += -1.0f;
-    }
-
-    if (!SINGLE_PLAYER) 
-    {
-        if (key_state[SDL_SCANCODE_UP])
+        if (key_state[SDL_SCANCODE_W])
         {
-            g_right_movement.y += 1.0f;
+            g_left_movement.y += 1.0f;
         }
-        if (key_state[SDL_SCANCODE_DOWN])
+        if (key_state[SDL_SCANCODE_S])
         {
-            g_right_movement.y += -1.0f;
+            g_left_movement.y += -1.0f;
+        }
+
+        if (!SINGLE_PLAYER)
+        {
+            if (key_state[SDL_SCANCODE_UP])
+            {
+                g_right_movement.y += 1.0f;
+            }
+            if (key_state[SDL_SCANCODE_DOWN])
+            {
+                g_right_movement.y += -1.0f;
+            }
         }
     }
 
@@ -280,9 +291,16 @@ void process_input()
 // function to restart so i can call call it multiple times
 void restart() {
     g_ball_position = glm::vec3(0.0f, 0.0f, 0.0f);
-    g_ball_movement.x = 1.0f;
-    g_ball_movement.y = 0.0f;
+    g_ball_movement.x = float(rand() % 101) - 50;
+    g_ball_movement.y = float(rand() % 101) - 50;
     g_ball_movement = glm::normalize(g_ball_movement);
+    GAME_STATE = 0;
+    std::cout << "g_ball_movement ";
+    coord(g_ball_movement);
+}
+
+void coord(glm::vec3& values) {
+    std::cout << "X: " << values.x << " Y: " << values.y << std::endl;
 }
 
 void update()
@@ -291,86 +309,99 @@ void update()
     float delta_time = ticks - g_previous_ticks; // the delta time is the difference from the last frame
     g_previous_ticks = ticks;
 
-    // Add direction * units per second * elapsed time
-    g_left_position += g_left_movement * g_paddle_speed * delta_time;
+    if (GAME_STATE == 0)
+    {    
+        g_ball_position += g_ball_movement * g_ball_speed * delta_time;
 
-    if (g_left_position.y > UPPER_BOUND) {
-        g_left_position.y = UPPER_BOUND;
+        if (g_ball_position.y > UPPER_BOUND)
+        {
+            g_ball_position.y = UPPER_BOUND;
+            g_ball_movement.y = -g_ball_movement.y;
+        }
+        if (g_ball_position.y < LOWER_BOUND)
+        {
+            g_ball_position.y = LOWER_BOUND;
+            g_ball_movement.y = -g_ball_movement.y;
+        }
+
+        g_ball_matrix = glm::mat4(1.0f);
+        g_ball_matrix = glm::translate(g_ball_matrix, INIT_POS_BALL);
+        g_ball_matrix = glm::translate(g_ball_matrix, g_ball_position);
+        g_ball_matrix = glm::scale(g_ball_matrix, INIT_SCALE_BALL);
+
+
+        // Add direction * units per second * elapsed time
+        g_left_position += g_left_movement * g_paddle_speed * delta_time;
+
+        if (g_left_position.y > UPPER_BOUND) {
+            g_left_position.y = UPPER_BOUND;
+        }
+        if (g_left_position.y < LOWER_BOUND) {
+            g_left_position.y = LOWER_BOUND;
+        }
+
+        g_left_matrix = glm::mat4(1.0f);
+        g_left_matrix = glm::translate(g_left_matrix, INIT_POS_LEFT);
+        g_left_matrix = glm::translate(g_left_matrix, g_left_position);
+        g_left_matrix = glm::scale(g_left_matrix, INIT_SCALE_LEFT);
+
+        if (SINGLE_PLAYER) {
+            g_right_movement.y = ((g_ball_position.y > g_right_position.y) ? 1.0f : -1.0f);
+        }
+
+        g_right_position += g_right_movement * g_paddle_speed * delta_time;
+
+        if (g_right_position.y > UPPER_BOUND) {
+            g_right_position.y = UPPER_BOUND;
+        }
+        if (g_right_position.y < LOWER_BOUND) {
+            g_right_position.y = LOWER_BOUND;
+        }
+
+        g_right_matrix = glm::mat4(1.0f);
+        g_right_matrix = glm::translate(g_right_matrix, INIT_POS_RIGHT);
+        g_right_matrix = glm::translate(g_right_matrix, g_right_position);
+        g_right_matrix = glm::scale(g_right_matrix, INIT_SCALE_RIGHT);
+
+        float ball_x = g_ball_position.x + INIT_POS_BALL.x;
+        float ball_y = g_ball_position.y + INIT_POS_BALL.y;
+
+        float left_x = g_left_position.x + INIT_POS_LEFT.x;
+        float left_y = g_left_position.y + INIT_POS_LEFT.y;
+
+        float right_x = g_right_position.x + INIT_POS_RIGHT.x;
+        float right_y = g_right_position.y + INIT_POS_RIGHT.y;
+
+        float x_dist_left = fabs(ball_x - left_x) - ((INIT_SCALE_BALL.x + INIT_SCALE_LEFT.x) / 2.0f);
+        float y_dist_left = fabs(ball_y - left_y) - ((INIT_SCALE_BALL.y + INIT_SCALE_LEFT.y) / 2.0f);
+
+        float x_dist_right = fabs(ball_x - right_x) - ((INIT_SCALE_BALL.x + INIT_SCALE_RIGHT.x) / 2.0f);
+        float y_dist_right = fabs(ball_y - right_y) - ((INIT_SCALE_BALL.y + INIT_SCALE_RIGHT.y) / 2.0f);
+
+        if (x_dist_left < 0.0f && y_dist_left < 0.0f) 
+        {
+            g_ball_movement = -g_ball_movement;
+        }
+
+        if (x_dist_right < 0.0f && y_dist_right < 0.0f) 
+        {
+            g_ball_movement = -g_ball_movement;
+        }
+
+        if (g_ball_position.x > WALL) 
+        {
+            GAME_STATE = 1;
+        }
+
+        if (g_ball_position.x < -WALL) {
+            GAME_STATE = 2;
+        }
     }
-    if (g_left_position.y < LOWER_BOUND) {
-        g_left_position.y = LOWER_BOUND;
-    }
-
-    g_left_matrix = glm::mat4(1.0f);
-    g_left_matrix = glm::translate(g_left_matrix, INIT_POS_LEFT);
-    g_left_matrix = glm::translate(g_left_matrix, g_left_position);
-    g_left_matrix = glm::scale(g_left_matrix, INIT_SCALE_LEFT);
-
-
-    g_right_position += g_right_movement * g_paddle_speed * delta_time;
-
-    if (g_right_position.y > UPPER_BOUND) {
-        g_right_position.y = UPPER_BOUND;
-    }
-    if (g_right_position.y < LOWER_BOUND) {
-        g_right_position.y = LOWER_BOUND;
-    }
-
-    g_right_matrix = glm::mat4(1.0f);
-    g_right_matrix = glm::translate(g_right_matrix, INIT_POS_RIGHT);
-    g_right_matrix = glm::translate(g_right_matrix, g_right_position);
-    g_right_matrix = glm::scale(g_right_matrix, INIT_SCALE_RIGHT);
-
-
-    g_ball_position += g_ball_movement * g_ball_speed * delta_time;
-
-    if (g_ball_position.y > UPPER_BOUND) 
+    else if (GAME_STATE != 0) 
     {
-        g_ball_position.y = UPPER_BOUND;
-        g_ball_movement.y = -g_ball_movement.y;
-    }
-    if (g_ball_position.y < LOWER_BOUND) 
-    {
-        g_ball_position.y = LOWER_BOUND;
-        g_ball_movement.y = -g_ball_movement.y;
-    }
-
-    g_ball_matrix = glm::mat4(1.0f);
-    g_ball_matrix = glm::translate(g_ball_matrix, INIT_POS_BALL);
-    g_ball_matrix = glm::translate(g_ball_matrix, g_ball_position);
-    g_ball_matrix = glm::scale(g_ball_matrix, INIT_SCALE_BALL);
-
-    float ball_x = g_ball_position.x + INIT_POS_BALL.x;
-    float ball_y = g_ball_position.y + INIT_POS_BALL.y;
-
-    float left_x = g_left_position.x + INIT_POS_LEFT.x;
-    float left_y = g_left_position.y + INIT_POS_LEFT.y;
-
-    float right_x = g_right_position.x + INIT_POS_RIGHT.x;
-    float right_y = g_right_position.y + INIT_POS_RIGHT.y;
-
-    float x_dist_left = fabs(ball_x - left_x) - ((INIT_SCALE_BALL.x + INIT_SCALE_LEFT.x) / 2.0f);
-    float y_dist_left = fabs(ball_y - left_y) - ((INIT_SCALE_BALL.y + INIT_SCALE_LEFT.y) / 2.0f);
-
-    float x_dist_right = fabs(ball_x - right_x) - ((INIT_SCALE_BALL.x + INIT_SCALE_RIGHT.x) / 2.0f);
-    float y_dist_right = fabs(ball_y - right_y) - ((INIT_SCALE_BALL.y + INIT_SCALE_RIGHT.y) / 2.0f);
-
-    if (x_dist_left < 0.0f && y_dist_left < 0.0f) {
-        g_ball_movement = -g_ball_movement;
-    }
-
-    if (x_dist_right < 0.0f && y_dist_right < 0.0f) {
-        g_ball_movement = -g_ball_movement;
-    }
-
-    if (g_ball_position.x > WALL) {
-        END_STATE = 1;
-        restart();
-    }
-
-    if (g_ball_position.x < -WALL) {
-        END_STATE = 2;
-        restart();
+        g_win_matrix = glm::mat4(1.0f);
+        g_win_matrix = glm::translate(g_win_matrix, INIT_POS_WIN);
+        g_win_matrix = glm::scale(g_win_matrix, INIT_SCALE_WIN);
     }
 
 
@@ -406,9 +437,21 @@ void render() {
     glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
 
     // Bind texture
-    draw_object(g_left_matrix, g_left_texture_id);
-    draw_object(g_right_matrix, g_right_texture_id);
-    draw_object(g_ball_matrix, g_ball_texture_id);
+    if (GAME_STATE == 0) 
+    {
+        draw_object(g_left_matrix, g_left_texture_id);
+        draw_object(g_right_matrix, g_right_texture_id);
+        draw_object(g_ball_matrix, g_ball_texture_id);
+    }
+    else if (GAME_STATE == 1)
+    {
+        draw_object(g_win_matrix, g_p1_texture_id);
+    }
+    else if (GAME_STATE == 2) 
+    {
+        draw_object(g_win_matrix, g_p2_texture_id);
+    }
+
 
     // We disable two attribute arrays now
     glDisableVertexAttribArray(g_shader_program.get_position_attribute());
